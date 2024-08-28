f89bf26d9e43fcd752498f3ec1dc892f7676c4a1
@azrlrsdi_ <azrieldepok12.com>
2024-08-28 21:31:57 +0700

    Menambahkan Folder

---
+++ routes/goalRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal } = require ("../controllers/goalController")
+
+router.get('/', getGoals)
+
+router.post('/', setGoal)
+
+
+
+module.exports = router
